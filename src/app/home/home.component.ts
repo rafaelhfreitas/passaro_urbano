27cc7f1c639d251f9506a8ddcea4b2f025090c81
Rafael Freitas <rafael@ezsolucoes.com.br>
2023-08-01 20:28:40 -0300

    Added promise to resolve getOfertas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { 
 
+  public ofertas: Oferta[]
+
+  constructor(private ofertasService: OfertasService) { 
   }
+
+  // ngOnInit() {
+  //   // this.ofertas = this.ofertasService.getOfertas();
+
+  //   this.ofertasService.getOfertasPromise().then(
+  //     (ofertas: Oferta[]) => { this.ofertas = ofertas },
+  //     ( response: any) => {console.log(response) }
+  //   )
+  // }
 
   ngOnInit() {
-    let ofertas: OfertasService = new OfertasService();
-    console.log(ofertas.getOfertas());
-  }
+    // this.ofertas = this.ofertasService.getOfertas();
+
+    this.ofertasService.getOfertasPromise().then(
+      (ofertas: Oferta[]) => { 
+        console.log('resolvido');
+        this.ofertas = ofertas; }
+    ).catch(
+      ( response: any) => {console.log(response)}
+    )
+  }  
 
 }
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,8 +1,72 @@
+import { Oferta } from "./shared/oferta.model";
+
 export class OfertasService {
 
-    public getOfertas(): Array<string> {
-        let ofertas = ['Oferta1', 'Oferta2', 'Oferta3']
-        return ofertas;
+    public ofertas: Oferta[] = [
+        {
+            id: 1,
+            categoria: "restaurante",
+            titulo: "Super Burguer",
+            descricao_oferta: "Rodizio de Mini burguers com opção de entrada",
+            anunciante: "Original Burguer",
+            valor: 29.90,
+            destaque: true,
+            imagens: [
+                {url: "/assets/ofertas/1/img1.jpg"},
+                {url: "/assets/ofertas/1/img2.jpg"},
+                {url: "/assets/ofertas/1/img3.jpg"},
+                {url: "/assets/ofertas/1/img4.jpg"}
+            ]
+        },
+        {
+            id: 2,
+            categoria: "restaurante",
+            titulo: "Cozinha Mexicana",
+            descricao_oferta: "Almoço ou Jantar com Rodízio Mexicano delicioso.",
+            anunciante: "Mexicana",
+            valor: 32.90,
+            destaque: true,
+            imagens: [
+                {url: "/assets/ofertas/2/img1.jpg"},
+                {url: "/assets/ofertas/2/img2.jpg"},
+                {url: "/assets/ofertas/2/img3.jpg"},
+                {url: "/assets/ofertas/2/img4.jpg"}
+            ]
+        
+        },
+        {
+            id: 4,
+            categoria: "diversao",
+            titulo: "Estância das águas",
+            descricao_oferta: "Diversão garantida com piscinas, trilhas e muito mais.",
+            anunciante: "Estância das águas",
+            valor: 31.90,
+            destaque: true,
+            imagens: [
+                {url: "/assets/ofertas/3/img1.jpg"},
+                {url: "/assets/ofertas/3/img2.jpg"},
+                {url: "/assets/ofertas/3/img3.jpg"},
+                {url: "/assets/ofertas/3/img4.jpg"},
+                {url: "/assets/ofertas/3/img5.jpg"},
+                {url: "/assets/ofertas/3/img6.jpg"}
+            ]
+        }               
+    ];
+
+    public getOfertas(): Array<Oferta> {
+        return this.ofertas;
+    }
+
+
+    public getOfertasPromise(): Promise<Oferta[]> {
+        return new Promise((resolve, reject) => {
+            let deu_certo = true;
+            if (deu_certo) {
+                setTimeout( () => resolve(this.ofertas) ,3000)                
+            } else {
+                reject({ codigo_erro: 404, mensagem_erro: "NOT_FOUND"})
+            } 
+        })
     }
 
 
