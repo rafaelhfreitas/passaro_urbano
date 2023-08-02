1072161528598a47eb0c11f72d3fb662036e87ce
Rafael Freitas <rafael.freitas@webrota.com.br>
2023-08-02 10:41:16 -0300

    First observable

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+
+import 'rxjs/Rx'
 
 @Component({
   selector: 'app-oferta',
@@ -31,11 +35,43 @@ export class OfertaComponent implements OnInit {
     })
 
 
+    /*
     this.route.params.subscribe(
       (param : any ) => { console.log(param) },
       (erro : any ) => console.log(erro),
       () => console.log('processamento classificado como concluído')
     )
+    */
+
+
+    /*
+    let time = Observable.interval(2000)
+
+    time.subscribe(( interval: number) => {
+      console.log(interval);
+    })
+    */
+
+
+    // observable (obserbavel)
+    let myObservable = Observable.create(
+      (observer: Observer<number>) => {
+        observer.next(1)
+        observer.next(3)
+        //observer.error('Algum erro foi encontrado no stream de evento')
+        observer.complete()
+        observer.next(3)
+    })
+
+
+
+    // observable (observador)
+    myObservable.subscribe(
+      (param: number) =>  console.log(param + 10),
+      (erro: string) => { console.log(erro)},
+      () => console.log('Stream de eventos foi finalizada')
+    )
+
   }
 
 }
