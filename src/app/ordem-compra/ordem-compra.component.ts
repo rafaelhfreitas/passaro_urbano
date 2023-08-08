9f4232fe3edeec5a10613dd123e505d7fd6e242e
Rafael Freitas <rafael@ezsolucoes.com.br>
2023-08-07 23:45:08 -0300

    Added a cart to place order to system

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 
-import { OrdemCompraService } from '../ordem-compra.service'
-import { Pedido } from '../shared/pedido.model'
+import { OrdemCompraService } from '../ordem-compra.service';
+import { CarrinhoService } from '../carrinho.service';
+
+import { Pedido } from '../shared/pedido.model';
+import { ItemCarrinho } from 'app/shared/item-carrinho.model';
 
 @Component({
   selector: 'app-ordem-compra',
@@ -14,6 +17,7 @@ import { Pedido } from '../shared/pedido.model'
 export class OrdemCompraComponent implements OnInit {
   
   public idPedidoCompra : number;
+  public itensCarrinho: ItemCarrinho[] = [];
   
   public formulario: FormGroup = new FormGroup({
     'endereco': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
@@ -22,38 +26,72 @@ export class OrdemCompraComponent implements OnInit {
     'formaPagamento': new FormControl(null, [Validators.required])
   });
 
-  constructor(private ordemCompraService: OrdemCompraService) { }
+  constructor(
+      private ordemCompraService: OrdemCompraService,
+      private carrinhoService: CarrinhoService) { }
 
   ngOnInit() {
+
+    // console.log('Itens carrinho: ', this.carrinhoService.exibirItens());
+    this.itensCarrinho =   this.carrinhoService.exibirItens();
+    console.log(this.itensCarrinho);
     
   }
 
   public confirmarCompra(): void {
     if (this.formulario.status === "INVALID") {
+
       console.log(this.formulario.status);
       this.formulario.get('endereco').markAsTouched();
       this.formulario.get('numero').markAsTouched();
       this.formulario.get('formaPagamento').markAsTouched();
+    
     } else {
-      console.log('formulário está válido ')
-      let pedido = new Pedido(
-          // this.formulario.get('endereco').value,
-          // this.formulario.get('numero').value,
-          // this.formulario.get('complemento').value,
-          // this.formulario.get('formaPagamento').value
-          this.formulario.value.endereco,
-          this.formulario.value.numero,
-          this.formulario.value.complemento,
-          this.formulario.value.formaPagamento
-      )
 
-      this.ordemCompraService.efetivarCompra(pedido).subscribe(
-        (idPedidoCompra: number) => {
-          // console.log(idPedidoCompra);
-          this.idPedidoCompra = idPedidoCompra;
+      if(this.carrinhoService.exibirItens().length === 0) {
+        
+        alert('Você não selecionou nenhum item');
 
-        }
-      )
+      } else {
+
+        console.log('formulário está válido ')
+        let pedido = new Pedido(
+            
+            // this.formulario.get('endereco').value,
+            // this.formulario.get('numero').value,
+            // this.formulario.get('complemento').value,
+            // this.formulario.get('formaPagamento').value
+  
+            this.formulario.value.endereco,
+            this.formulario.value.numero,
+            this.formulario.value.complemento,
+            this.formulario.value.formaPagamento,
+            this.carrinhoService.exibirItens()
+        );
+
+
+        this.ordemCompraService.efetivarCompra(pedido).subscribe(
+          (idPedidoCompra: number) => {
+            // console.log(idPedidoCompra);
+            this.idPedidoCompra = idPedidoCompra;
+            this.carrinhoService.limparCarrinho();
+  
+          });
+      }
+
+
     }
   }
+
+  public adicionar(item: ItemCarrinho):void {
+    this.carrinhoService.adicionarQuantidade(item);
+
+  }
+
+  public diminuir(item: ItemCarrinho):void {
+    this.carrinhoService.diminuirQuantidade(item);
+
+  }
+
+
 }
---
+++ src/app/shared/item-carrinho.model.ts
@@ -0,0 +1,13 @@
+class ItemCarrinho {
+    constructor(
+        public id: number,
+        public img: object,
+        public titulo: string,
+        public descricaoOferta: string,
+        public valor: number,
+        public quantidade: number
+    ) {}
+}
+
+
+export { ItemCarrinho }
